c8750a8134b5d4b6af84461bed338a3750a41695
Wojtaszek Michał <michal.wojtaszek@microsoft.wsei.edu.pl>
2019-03-30 18:33:19 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj !");
+            Console.Write("Podaj swoje imie : ");
+            string x = Console.ReadLine();
+            Console.WriteLine($"Witaj, {x}");
+
+            Random generator = new Random();
+            int randomNumber = generator.Next(1, 101);
+            Console.WriteLine("Wylosowano liczbe od 1 do 100. \n Odganij ja :");
+        }
+    }
+}
